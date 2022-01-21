c27e4db738f6ffd9181dd34009582432191c4c62
Deepak <deepakdeepakpandey25@gmail.com>
2022-01-21 22:59:47 +0530

    init

--- STL/vector2.cc
+++ STL/vector2.cc
@@ -3,27 +3,24 @@
 using namespace std;
 int main()
 {
-    vector <int> v,v1,v2;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    cout<<v[0]<<endl;
-    cout<<v.size()<<endl;
-    cout<<v.at(0)<<"\n";
-    //To print first and last
-    cout<<v.front()<<" "<<v.back()<<endl;
-    //To get the max element
-    cout<<v.max_size()<<endl;
-    //To swap elements
-    v1.swap(v);
-    cout<<v1[0]<<endl;
-    //To clear the vector
-    v1.clear();
-    cout<<v1[0]<<endl;
-    if(v1.empty())
+    vector <int> v(5);
+    for(int i=0;i<5;++i)
     {
-        cout<<"Vector is empty\n";
+        cin>>v[i];
     }
+    //Use of iterator
+    for(auto it=v.begin();it!=v.end();it++)
+    {
+        cout<<*it;
+    }
+    cout<<endl;
+    //Print in reverse order
+    for(auto it=v.rbegin();it!=v.rend();it++)
+    {
+        cout<<*it;
+    }
+    cout<<endl;
+
 
 
 }
